16470860687ac53e64d69b687d5ce56c6dbadadb
egoo00 <.egooo914@gmail.com>
2025-05-13 17:25:47 +0300

    remake lab01

---
+++ src/main/java/controller/DistanceController.java
@@ -0,0 +1,37 @@
+package com.example.distanceservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DistanceController {
+
+    private final Map<String, double[]> cityCoordinates = Map.of(
+            "Minsk", new double[]{53.9, 27.5667},
+            "Moscow", new double[]{55.7558, 37.6178},
+            "Warsaw", new double[]{52.2297, 21.0122}
+    );
+
+    @GetMapping("/distance")
+    public Map<String, Object> calculateDistance(
+            @RequestParam String city1,
+            @RequestParam String city2) {
+
+        Map<String, Object> response = new HashMap<>();
+        if (!cityCoordinates.containsKey(city1) || !cityCoordinates.containsKey(city2)) {
+            response.put("error", "City not found");
+            return response;
+        }
+
+        double distance = 666; // Пример хардкода
+        response.put("city1", city1);
+        response.put("city2", city2);
+        response.put("distance", distance);
+        response.put("unit", "km");
+        return response;
+    }
+}
